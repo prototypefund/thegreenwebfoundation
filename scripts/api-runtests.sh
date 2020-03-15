431fff0c63ec62b66e97b22de17facdb49d28cbd
Arend-Jan Tetteroo <aj@arendjantetteroo.nl>
2020-03-15 15:54:18 +0100

    Run database fixtures before api tests

--- scripts/api-runtests.sh
+++ scripts/api-runtests.sh
@@ -1,11 +1,13 @@
 # Run package tests
 composer install
 
+composer install -d packages/greencheck
+
 # clear the test db
-#php ./tests/doctrine-cli.php orm:schema-tool:drop --force
+php ./packages/greencheck/tests/doctrine-cli.php orm:schema-tool:drop --force
 
 # create the test db
-#php ./tests/doctrine-cli.php orm:schema-tool:create
+php ./packages/greencheck/tests/doctrine-cli.php orm:schema-tool:create
 
 # run the api tests
 ./vendor/bin/simple-phpunit -c phpunit.xml.dist
